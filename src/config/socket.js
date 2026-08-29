bfe4fb962a656bac95e9a0693a900b7da1e65ab0
ritesh329 <145488174+ritesh329@users.noreply.github.com>
2026-08-30 01:10:23 +0530

    Update socket CORS origin for production

--- src/config/socket.js
+++ src/config/socket.js
@@ -43,7 +43,7 @@ dotenv.config();
 export const configureSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: 'https://frontendchatapp-k21l.onrender.com',
 
       credentials: true,
 
